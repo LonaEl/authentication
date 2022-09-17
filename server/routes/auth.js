df5c4049adeb08222b80cc937ccd607284322d9f
Lona Sontsele <1867792@students.wits.ac.za>
2022-09-17 03:59:53 +0200

    Lona

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,9 @@ import express from 'express'
 import { User } from "../models/user.js";
 import  bcrypt from "bcrypt";
 import Joi from "joi";
+import Token from "../models/token";
+import crypto from "crypto";
+import sendEmail from "../utils/sendEmail";
 
 const router = express.Router();
 
@@ -21,6 +24,22 @@ router.post("/", async (req, res) => {
 		);
 		if (!validPassword)
 			return res.status(401).send({ message: "Invalid Email or Password" });
+
+		if (!user.verified) {
+			let token = await Token.findOne({ userId: user._id });
+			if (!token) {
+				token = await new Token({
+					userId: user._id,
+					token: crypto.randomBytes(32).toString("hex"),
+				}).save();
+				const url = `${process.env.BASE_URL}users/${user.id}/verify/${token.token}`;
+				await sendEmail(user.email, "Verify Email", url);
+			}
+
+			return res
+				.status(400)
+				.send({ message: "An Email sent to your account please verify" });
+		}
 
 		const token = user.generateAuthToken();
 		res.status(200).send({ data: token, message: "logged in successfully" });
